4adb86f1809a9810b8f036d47578b31603d8db80
ctomris59-dev <ctomris59@gmail.com>
2026-08-17 17:34:29 +0300

    Load V4.5 game-first styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./v5-avatar.css";
 import "./v5-focus.css";
+import "./v45-game.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
