462a9a32dfdbe638b699643015455f6d8383d4a1
ctomris59-dev <ctomris59@gmail.com>
2026-08-17 16:26:33 +0300

    V5 enable animated avatar stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./v44-avatar-rig.css";
+import "./v5-avatar.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
