4c9c80793f6a2596d0787a177e7de77094a2419f
ctomris59-dev <ctomris59@gmail.com>
2026-08-18 16:07:34 +0300

    V4.9 load fitted character rendering styles

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./v5-focus.css";
 import "./v47-reference.css";
 import "./v47-stage.css";
 import "./v47-extras.css";
-import "./v48-wardrobe.css";
+import "./v49-wardrobe.css";
 import V47App from "./V47App.jsx";
 
 createRoot(document.getElementById("root")).render(
