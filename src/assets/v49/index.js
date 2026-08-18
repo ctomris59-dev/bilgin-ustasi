c2d849d7a6add5d5a23d8df4a3895011c65ba8e5
ctomris59-dev <ctomris59@gmail.com>
2026-08-18 16:10:46 +0300

    V4.9 use exact fitted set renders as item previews

--- src/assets/v49/index.js
+++ src/assets/v49/index.js
@@ -1,14 +1,11 @@
 import explorerFull from "./explorerFull.js";
-import explorerItems from "./explorerItems.js";
 import cloudFull from "./cloudFull.js";
-import cloudItems from "./cloudItems.js";
 import forestFull from "./forestFull.js";
-import forestItems from "./forestItems.js";
 
 export const V49_SET_VISUALS = Object.freeze({
-  explorer: Object.freeze({ full: explorerFull, ...explorerItems }),
-  cloud: Object.freeze({ full: cloudFull, ...cloudItems }),
-  forest: Object.freeze({ full: forestFull, ...forestItems }),
+  explorer: Object.freeze({ full: explorerFull }),
+  cloud: Object.freeze({ full: cloudFull }),
+  forest: Object.freeze({ full: forestFull }),
 });
 export function getV49SetVisual(setId="explorer"){return V49_SET_VISUALS[setId]||V49_SET_VISUALS.explorer;}
-export function getV49ItemVisual(item={}){const set=getV49SetVisual(item.set);return set?.[item.slot]||set?.outfit||"";}
+export function getV49ItemVisual(item={}){return getV49SetVisual(item.set)?.full||V49_SET_VISUALS.explorer.full;}
