f0327bad5c5eee69aa82d31a80d942395b601e23
ctomris59-dev <ctomris59@gmail.com>
2026-08-19 10:30:38 +0300

    V4.9.2 use safe explorer HD chunks

--- src/assets/v492q25/explorer.js
+++ src/assets/v492q25/explorer.js
@@ -1,6 +1,8 @@
 import c0 from "./explorer0.js";
 import c1 from "./explorer1.js";
-import c2a from "./explorer2a.js";
-import c2b from "./explorer2b.js";
+import c20 from "./explorer2_0.js";
+import c21 from "./explorer2_1.js";
+import c22 from "./explorer2_2.js";
+import c23 from "./explorer2_3.js";
 import c3 from "./explorer3.js";
-export default `data:image/avif;base64,${c0}${c1}${c2a}${c2b}${c3}`;
+export default `data:image/avif;base64,${c0}${c1}${c20}${c21}${c22}${c23}${c3}`;
