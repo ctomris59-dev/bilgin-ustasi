88cf3a5bca33b7c3799160a64048e6e18053e4da
ctomris59-dev <ctomris59@gmail.com>
2026-08-19 10:17:06 +0300

    V4.9.2 assemble forest HD master

---
+++ src/assets/v492/forest.js
@@ -0,0 +1,9 @@
+import c0 from "./forestHdChunk0.js";
+import c1 from "./forestHdChunk1.js";
+import c2a from "./forestHdChunk2a.js";
+import c2b from "./forestHdChunk2b.js";
+import c2c from "./forestHdChunk2c.js";
+import c2d from "./forestHdChunk2d.js";
+import c3a from "./forestHdChunk3a.js";
+import c3b from "./forestHdChunk3b.js";
+export default `data:image/avif;base64,${c0}${c1}${c2a}${c2b}${c2c}${c2d}${c3a}${c3b}`;
