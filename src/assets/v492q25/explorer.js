5466dad9faf079b6a493864a18106129d41a25da
ctomris59-dev <ctomris59@gmail.com>
2026-08-19 10:22:04 +0300

    V4.9.2 assemble explorer HD from validated chunks

--- src/assets/v492q25/explorer.js
+++ src/assets/v492q25/explorer.js
@@ -1,5 +1,6 @@
 import c0 from "./explorer0.js";
 import c1 from "./explorer1.js";
-import c2 from "./explorer2.js";
+import c2a from "./explorer2a.js";
+import c2b from "./explorer2b.js";
 import c3 from "./explorer3.js";
-export default `data:image/avif;base64,${c0}${c1}${c2}${c3}`;
+export default `data:image/avif;base64,${c0}${c1}${c2a}${c2b}${c3}`;
