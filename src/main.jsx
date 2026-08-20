51e212cb69a941500016f4e8d2c2913276404b33
ctomris59-dev <ctomris59@gmail.com>
2026-08-20 09:31:48 +0300

    Activate V5 styles and remove old wardrobe CSS

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,8 @@ import "./v5-focus.css";
 import "./v47-reference.css";
 import "./v47-stage.css";
 import "./v47-extras.css";
-import "./v49-wardrobe.css";
 import "./v493-screen-fit.css";
-import "./v494-tryon.css";
+import "./v50-styles.css";
 import V47App from "./V47App.jsx";
 
 createRoot(document.getElementById("root")).render(
